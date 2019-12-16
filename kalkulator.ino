d8eb972008cdfcaf91565fbfe8f87e634bc4ae5f
xFiliOpx <53794727+xFiliOpx@users.noreply.github.com>
2019-12-16 14:43:30 +0100

    Add files via upload

--- kalkulator.ino
+++ kalkulator.ino
@@ -1,6 +1,6 @@
 #include "Nokia_5110.h"
 
-#define RST 2 //definiowanie pinow z wyswietlacza
+#define RST 2 //definiowanie pinow z wyswietlacza XDXDtestXDXD
 #define CE 3
 #define DC 4
 #define DIN 5
@@ -8,32 +8,24 @@
 
 Nokia_5110 lcd = Nokia_5110(RST, CE, DC, DIN, CLK); //ustawianie wyswietlacza
 
-String nacisnieto();{ //(tymczasowo z portu szeregowego) pobieranie nast cyfry/dzialania
-  String x = Serial.readString();
-  return x;
-  }
-
+String nastCyfr, caleDzialanie;
 
 void setup() {
   lcd.print("Wait...");
-  String nastCyfr, caleDzialanie;
   Serial.begin(9600);
   lcd.clear();
 }
 
 void loop() {
-  nastCyfr = nacisnieto();
+  
+  nastCyfr = Serial.readString(); //tymczasowe wpisywanie nast cyfr
 
   if (nastCyfr != "="){ //sprawdzenie czy wykonac juz dzialanie jezeli jest =
-    caleDzalanie += nastCyfr;
+    caleDzialanie += nastCyfr;
     }
-  else { //tutaj bedzie cale sprawdzanie kolejnosci dialan i ich wykonywanie
     
+  else { //tutaj bedzie cale sprawdzanie kolejnosci dialan i ich wykonywanie
+    lcd.print(caleDzialanie);
     }
-  
-    lcd.clear();//to cale na puzniej zeby wyswietlac
-    a = Serial.readString();
-    lcd.print("Wpisane: ");
-    lcd.print(a);
-    delay(500);
+    
 }
